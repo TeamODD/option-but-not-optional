c528ae07630fe4f356e649373b761182e5549c58
camilie2 <gmsaturn0917@gmail.com>
2024-11-23 17:35:21 +0900

    Settings UI put
    Settings UI put

--- Assets/Scripts/Settings/Setting.cs
+++ Assets/Scripts/Settings/Setting.cs
@@ -7,6 +7,14 @@ namespace Settings
         private static Setting _instance;
         public GameObject settingPanel;
 
+        [SerializeField] private GameObject graphicBox;
+
+        [SerializeField] private GameObject soundBox;
+
+        [SerializeField] private GameObject systemBox;
+
+        private SettingType _currentSettingType = SettingType.Graphic;
+
         private UIState _currentState = UIState.Close;
 
         private void Awake()
@@ -25,6 +33,21 @@ namespace Settings
         {
             _currentState = _currentState == UIState.Open ? UIState.Close : UIState.Open;
             settingPanel.SetActive(_currentState == UIState.Open);
+        }
+
+        public void ChangeSettingType(int type)
+        {
+            _currentSettingType = (SettingType)type;
+            graphicBox.SetActive(_currentSettingType == SettingType.Graphic);
+            soundBox.SetActive(_currentSettingType == SettingType.Sound);
+            systemBox.SetActive(_currentSettingType == SettingType.System);
+        }
+
+        private enum SettingType
+        {
+            Graphic,
+            Sound,
+            System
         }
 
         private enum UIState
